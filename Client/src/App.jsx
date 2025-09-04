0cdb5217af2b358933a5f403d7fc11b246d140c4
Nehmiya <nehmiyatech@gmail.com>
2025-09-04 18:53:26 +0300

    Creating React App & Setting up React Router Dom

---
+++ Client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route,Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Login from './Pages/Login/Login'
+import Register from './Pages/Register/Register'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App
